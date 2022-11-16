c684e2dfd3165f0ec42ff02b9ffb0b80b8705001
Ismat Mashaba <ismat.mashaba96@gmail.com>
2022-11-16 23:14:28 +0100

    Ready to review

---
+++ fetchData.js
@@ -0,0 +1,28 @@
+
+let url='https://api.escuelajs.co/api/v1/products'
+
+fetch(url)
+    .then(res => res.json())
+    .then(allData => {
+
+        for (let i = 0; i < 10 ; i++) {
+            let data = allData[i];
+            let title = data.title;
+            let image = data.category.image;
+            let description = data.description;
+
+            let myBoxContent = document.createElement("div");
+
+            myBoxContent.innerHTML = `<div class="box-content">
+            <h1 class="title">Title: ${title}</h1>
+            <img class="image" src="${image}">
+            <p class="description">Description: ${description}</p>
+            </div>`
+
+            let box = document.getElementById("box");
+            box.appendChild(myBoxContent)
+        }
+    }
+    )
+    .catch((error)=>console.log(error))
+
